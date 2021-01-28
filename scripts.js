21a56f5aa08d62ee8baa1fd8bbf1c8c8289fa947
VigilV11 <vigilv@pm.me>
2021-01-28 21:06:55 +0530

    Get location from user click on map. Add pin to location. Basic modal popup.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { mapAPI, mapAttribution } from './constants.js';
 
+// For Parcel HMR
+// module.hot && module.hot.accept();
+
 //++++++++++++++++ PROMISIFYING GEOLOCATION API CALL ++++++++++++++++\\
 const getPosition = function() {
   return new Promise((resolve, reject) =>
@@ -17,9 +20,31 @@ const loadMap = async function() {
   const lng = res.coords.longitude;
 
   console.log(lat, lng);
-  let mymap = L.map('mapid').setView([lat, lng], 13);
+  let mapObj = L.map('mapid').setView([lat, lng], 13);
 
-  L.tileLayer(mapAPI, mapAttribution).addTo(mymap);
+  L.tileLayer(mapAPI, mapAttribution).addTo(mapObj);
+
+  return mapObj;
 };
 
-loadMap();
+//++++++++++++++++ GET USER CLICK LOCATION ++++++++++++++++\\
+
+const getMapLocation = async function() {
+  const myMap = await loadMap();
+  myMap.on('click', onMapClick.bind(myMap));
+};
+
+function onMapClick(e) {
+  console.log(e.latlng);
+  console.log(this);
+  const myMap = this;
+  var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(myMap);
+  marker.bindPopup('<b>New location</b><br>how wonderful!').openPopup();
+  displayUserForm();
+}
+
+const displayUserForm = function() {
+  document.querySelector('.modal').style.visibility = 'visible';
+};
+
+getMapLocation();
--- todo.md
+++ todo.md
@@ -1,3 +1,9 @@
+### [2 git] "Get location from user click on map. Add pin to location. Basic modal popup."
+
+- [x] Get location (lat, lng) of user click from map
+- [x] Add a pin to the location; and popup
+- [x] Open a modal
+
 ### [1 git] "Promisified geoLocation. Installed dev env and leaflet. Render map to user's location."
 
 - [x] Promisify geoLocation and get current position
